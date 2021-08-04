e8ef370cae7279f7c1927e8cf4ef8b8003cdf909
Dian Tri Budianto <dyanzzz@gmail.com>
2021-08-04 23:00:18 +0700

    implement adapter, viewmodel, viewBinding in MainActivity

--- app/src/main/java/com/kotlin/appretrofit/MainActivity.kt
+++ app/src/main/java/com/kotlin/appretrofit/MainActivity.kt
@@ -2,10 +2,39 @@ package com.kotlin.appretrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kotlin.appretrofit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: PostViewModel
+    private lateinit var adapter: PostAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = PostAdapter()
+        adapter.notifyDataSetChanged()
+
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(PostViewModel::class.java)
+
+        binding.apply {
+            tvResponseCode.text = "JSON Placeholder"
+            rvPost.setHasFixedSize(true)
+            rvPost.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvPost.adapter = adapter
+            viewModel.setPostData()
+        }
+
+        viewModel.getPostData().observe(this, {
+            if (it != null) {
+                adapter.setList(it)
+            }
+        })
     }
 }
