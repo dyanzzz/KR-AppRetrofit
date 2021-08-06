8008ce9a49490e123a5636efafec1194f236f1e2
Dian Tri Budianto <dyanzzz@gmail.com>
2021-08-06 08:05:41 +0700

    Update PostViewModel to add fun createPostData

--- app/src/main/java/com/kotlin/appretrofit/PostViewModel.kt
+++ app/src/main/java/com/kotlin/appretrofit/PostViewModel.kt
@@ -3,7 +3,6 @@ package com.kotlin.appretrofit
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.kotlin.appretrofit.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,6 +11,8 @@ class PostViewModel: ViewModel() {
 
     // ini merupakan mutable live data yg merupakan arraylist dari data post
     val listPost = MutableLiveData<ArrayList<PostResponse>>()
+    val listCreatePost = MutableLiveData<CreatePostResponse>()
+    private var responseCode: Int = 0
 
     // function setter
     fun setPostData() {
@@ -24,17 +25,56 @@ class PostViewModel: ViewModel() {
                 ) {
                     if (response.isSuccessful) {
                         listPost.postValue(response.body())
+                        responseCode = response.code()
                     }
-                    println("Response : ${response.body()}")
+                    println("### Response : ${response.code()}")
                 }
 
                 override fun onFailure(call: Call<ArrayList<PostResponse>>, t: Throwable) {
-                    println("Failure : ${t.message}")
+                    println("##### Failure : ${t.message}")
                 }
             })
     }
 
+    fun createPostData() {
+        RetrofitClient.apiInstance
+            .createPost(
+                29,
+                "Retrofit tutorial",
+                "retrofit body text"
+            )
+            .enqueue(object : Callback<CreatePostResponse>{
+                override fun onResponse(
+                    call: Call<CreatePostResponse>,
+                    response: Response<CreatePostResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        listCreatePost.postValue(response.body())
+                        responseCode = response.code()
+                    }
+                    println("### Response : ${response.body()}")
+
+                }
+
+                override fun onFailure(call: Call<CreatePostResponse>, t: Throwable) {
+                    println("##### Failure : ${t.message}")
+                }
+
+            })
+    }
+
+    fun getResponseCode(): Int {
+        println("##### ResponseCode $responseCode")
+        return responseCode
+    }
+
+    fun getCreatePostData(): LiveData<CreatePostResponse>{
+        println("### getCreatePostData $listCreatePost")
+        return listCreatePost
+    }
+
     fun getPostData(): LiveData<ArrayList<PostResponse>>{
+        println("##### PostData $listPost")
         return listPost
     }
 }
